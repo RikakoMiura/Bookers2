ce0cb18b6d1d2be913fe7a12c1f543b29c6a9f15
RikakoMiura <rmiura2016@gmail.com>
2021-10-09 08:38:37 +0000

    [Update]Bookers2

--- Gemfile
+++ Gemfile
@@ -70,3 +70,11 @@ gem 'jquery-rails'
 gem 'font-awesome-sass','~>5.13'
 
 gem 'kaminari','~>1.2.1'
+
+group:test do
+  gem 'capybara','>=2.15'
+  gem 'rspec-rails'
+  gem "factory_bot_rails"
+  gem 'faker'
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
 
-  get '/home/about' => "homes#index"
-  post '/users/:id/edit' => 'users#create' 
+  get "home/about" ,to: 'homes#new',as: 'home_about'
+  post '/users/:id/edit' => 'users#create'
   patch 'users/:id/edit' => 'users#create', as:'update_user'
   post 'users/:id' => 'users#create'
   post 'books/:id' => 'books#create'
   post 'users/:id' => 'users#index'
 
 
-  
+
 
 
 
